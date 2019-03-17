4d92c830a935894ca2f7daac033d4836f0d6e84e
martin <martin.antalic@gmail.com>
2019-03-17 07:29:08 +0100

    adjustments to terraform deployment

--- tutorial-00/ansible-az-lab/main.tf
+++ tutorial-00/ansible-az-lab/main.tf
@@ -1,3 +1,8 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.resource_group}"
+  location = "${var.location}"
+
+}
 
 module "network" {
     source              = "./module/vnet"
@@ -12,11 +17,31 @@ module "network" {
                           }
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = "${var.resource_group}"
-  location = "${var.location}"
+#Azure Generic vNet Module
+
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.vnet_name}"
+  location            = "${var.location}"
+  address_space       = ["${var.address_space}"]
+  resource_group_name = "${var.resource_group}"
 
 }
+
+resource "azurerm_subnet" "subnet_pub" {
+  name                 = "private_subnet"
+  virtual_network_name = "${var.vnet_name}"
+  resource_group_name  = "${var.resource_group}"
+  address_prefix       = "10.0.100.0/24"
+  }
+
+resource "azurerm_subnet" "subnet_priv" {
+  name                 = "private_subnet"
+  virtual_network_name = "${var.vnet_name}"
+  resource_group_name  = "${var.resource_group}"
+  address_prefix       = "10.0.1.0/24"
+}
+
 
 resource "azurerm_network_security_group" "sg" {
   name                = "ansible-az-sg"
@@ -50,7 +75,7 @@ resource "azurerm_network_security_rule" "test" {
     vm_os_publisher     = "Canonical"
     vm_os_offer         = "UbuntuServer"
     vm_os_sku           = "18.04-LTS"
-    vnet_subnet_id      = "${module.network.subnet_names[0] }"
+    vnet_subnet_id      = "${ azurerm_subnet.subnet_priv.id}"
     boot_diagnostics    = "true"
     delete_os_disk_on_termination = "true"
 
--- tutorial-00/ansible-az-lab/var.tf
+++ tutorial-00/ansible-az-lab/var.tf
@@ -7,3 +7,23 @@ variable "resource_group" {
  default = "ansible-az-lab"
 }
 
+
+variable "vnet_name" {
+ default = "ansible-lab-vnet"
+}
+
+variable "dns_servers" {
+  description = "The DNS servers to be used with vNet."
+  default     = []
+}
+
+variable "address_space" {
+  description = "The address space that is used by the virtual network."
+  default     = "10.0.0.0/16"
+}
+
+
+variable "subnet_names" {
+  description = "A list of public subnets inside the vNet."
+  default     = ["subnet1"]
+}
